781480c6145dd772f20ab51263e8d5369712a67a
SNFBADGE2 <151949319+SNFBADGE2@users.noreply.github.com>
2023-11-27 19:53:40 +0100

    Update streamlit_app.py
    inserted additional row,
    added one header and tree text lines
    added a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale 3, Spinach & Rocket Smoothie')
+streamit.text('Hard Boiled Omega 3 & Free-Range Egg')
