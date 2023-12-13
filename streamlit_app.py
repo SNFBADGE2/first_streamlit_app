e1f5009770b4b9afcb27d706994a2da0e021ffb8
SNFBADGE2 <151949319+SNFBADGE2@users.noreply.github.com>
2023-12-13 08:57:14 +0100

    Update streamlit_app.py
    moved streamlit.stop() function to line 60

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ try:
 #streamlit.text(fruityvice_response.json()) #just writes data to the screen
 
 streamlit.header("The fruit load list contains:")
-#Snowflake related functions
+#Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list") 
@@ -57,6 +57,7 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+streamlit.stop()
 
 except URLError as e:
   streamlit.error()
@@ -64,7 +65,7 @@ except URLError as e:
 #take the json version of the response and normalize it 
 #output it to the screen as table
 #don't run anything past here while we troubleshoot
-streamlit.stop()
+
 
 #import snowflake.connector
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
