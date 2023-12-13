16a7cf4cce430b326a3c0ae2a64b61aadaeae2b1
SNFBADGE2 <151949319+SNFBADGE2@users.noreply.github.com>
2023-12-13 09:33:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ try:
      
 #streamlit.text(fruityvice_response.json()) #just writes data to the screen
 
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
