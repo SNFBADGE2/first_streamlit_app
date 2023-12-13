1443110c3cf26974e88cf411a1cbb45553429ade
SNFBADGE2 <151949319+SNFBADGE2@users.noreply.github.com>
2023-12-13 09:37:09 +0100

    Update streamlit_app.py
    added second streamlit.stop() in line 54

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,12 +45,13 @@ try:
      
 #streamlit.text(fruityvice_response.json()) #just writes data to the screen
 
-#streamlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list") 
     return my_cur.fetchall()
+streamlit.stop()
 
 #Add a button to load the fruit
 #if streamlit.button('Get Fruit Load List'):
