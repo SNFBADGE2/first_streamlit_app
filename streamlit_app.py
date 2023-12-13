21d495eafbdf9b42d34a4e2d9f7246d6ef7f3eb5
SNFBADGE2 <151949319+SNFBADGE2@users.noreply.github.com>
2023-12-13 09:04:31 +0100

    Update streamlit_app.py
    investigating error

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ try:
      
 #streamlit.text(fruityvice_response.json()) #just writes data to the screen
 
-streamlit.header('The fruit load list contains:')
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -53,10 +53,10 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
+#if streamlit.button('Get Fruit Load List'):
+#    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#    my_data_rows = get_fruit_load_list()
+#    streamlit.dataframe(my_data_rows)
 streamlit.stop()
 
 except URLError as e:
